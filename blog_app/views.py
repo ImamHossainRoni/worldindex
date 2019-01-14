82e15215c891d3f80a258f55ae1afa8a6b9efde7
imamhossainroni <imamhossainroni95@gmail.com>
2019-01-15 02:48:20 +0600

    bootstrap cdn added for templeting

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,4 +1,23 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render
 # Create your views here.
+posts = [
+    {
+        'author' : 'Imam',
+        'title'  : 'Blog post-1',
+        'content': 'first blog post',
+        'date_posted' : '20 august,2018'
+    },
+    {
+        'author' : 'Al Sweigart',
+        'title'  : 'Blog post-2',
+        'content': 'Second blog post',
+        'date_posted' : '22 august,2018'
+    }
+]
+context ={
+    'posts':posts
+}
 def home(request):
-    return HttpResponse("Hello World")
+    return render(request,'home.html',context)
+def about(request):
+    return render(request,'about.html',{'title':'about'})
