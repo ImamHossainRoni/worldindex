6dcdc41de2cb258447aac6a11c2f5c694109c013
imamhossainroni <imamhossainroni95@gmail.com>
2019-01-18 23:56:20 +0600

    working with django signals

--- users/signals.py
+++ users/signals.py
@@ -1,14 +1,8 @@
-from django.db import models
-from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 from django.dispatch import receiver
-# Create your models here.
-class Profile(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
-    image = models.ImageField(default = 'default.jpg',upload_to='profile_pics')
+from .models import Profile
 
-    def __str__(self):
-        return f'{self.user.username} Profile'
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
