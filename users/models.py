5d4c9198af2fca35bcba33eeddfcb2c0281f8deb
imamhossainroni <imamhossainroni95@gmail.com>
2019-01-19 23:26:41 +0600

    User profile updated successfully

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
@@ -10,15 +11,14 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
+    ''' Signal'''
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
-        print("yes 0000")
     else:
         instance.profile.save()
-        print("no 0000")
-
+      
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
