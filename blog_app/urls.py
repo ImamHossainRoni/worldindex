e4740658c000f1769b5fbd5a2a6e8c69d3328cfe
imamhossainroni <imamhossainroni95@gmail.com>
2019-01-21 22:57:29 +0600

    worked with Detailview && CreateView

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import PostListView
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+)
 from .import views
 urlpatterns = [
     path('',PostListView.as_view(),name = 'home-view'),
+    path('post/<int:pk>/',PostDetailView.as_view(),name = 'detail-view'),
+    path('post/new/',PostCreateView.as_view(),name = 'create-view'),
     path('about',views.about,name = 'about-view'),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    ) 
 # Create your views here.
 
 def home(request):
@@ -15,6 +20,18 @@ class PostListView(ListView):
     context_object_name = 'posts'
     ordering = '-date_posted'
 
+class PostDetailView(DetailView):
+     model = Post
+     template_name = 'post_detail.html'
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model = Post
+    fields = ['title','content']
+    template_name = 'post_form.html'
+
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 def about(request):
     return render(request,'about.html',{'title':'about'}) 
